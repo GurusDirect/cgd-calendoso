363476dd169993da77c7f87b1833501df2a2878c
Aivils Štoss <aivils@ithouse.lv>
2022-04-12 15:07:46 +0300

    [DCG-25] keycloak logout

--- components/Shell.tsx
+++ components/Shell.tsx
@@ -297,7 +297,9 @@ function UserDropdown({ small }: { small?: boolean }) {
   return user ? (
     <div className="flex items-center space-x-2 cursor-pointer group">
       <a
-        onClick={() => signOut({ callbackUrl: "/auth/logout" })}
+        onClick={() =>
+          signOut({ redirect: false }).then(() => (window.location.href = "/api/auth/federated-logout"))
+        }
         className="flex px-4 py-2 text-sm cursor-pointer hover:bg-gray-100 hover:text-gray-900">
         <LogoutIcon
           className={classNames("text-gray-500 group-hover:text-gray-700", "mr-2 flex-shrink-0 h-5 w-5")}
